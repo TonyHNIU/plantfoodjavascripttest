208655c55d72a0aab3b92aae9d0231ab63b577a5
HUI NIU <hui-niu@outlook.com>
2021-11-05 22:21:54 +1300

    add a few libs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div>Learn React</div>;
+  return (
+    <div className="main">
+      <div>
+        <h3>Login</h3>
+      </div>
+    </div>
+  );
 }
 
 export default App;
